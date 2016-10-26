d53430d91b2127f787abf245480e27ca42d2937b
ruben.higuera <ruben.higuera@WKSALMPBB706.u-tad.es>
2016-10-26 12:42:38 +0200

    comenzada conexion con bbdd

--- src/accesoDatos/AccesoBBDD.java
+++ src/accesoDatos/AccesoBBDD.java
@@ -1,12 +1,41 @@
 package accesoDatos;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
+
+import com.sun.corba.se.pept.transport.Connection;
 
 import logicaRefrescos.Deposito;
 import logicaRefrescos.Dispensador;
 
-public  class AccesoBBDD implements Datos{
-	
+public class AccesoBBDD implements Datos {
+
+	private Connection conexion;
+
+	private String bd, login, url, pwd;
+
+	public AccesoBBDD() {
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			bd = "maquinarefrescos";
+			login = "root";
+			url = "jdbc:mysql://LocalHost/" + bd;
+			pwd = "root";
+			try {
+				conexion = (Connection) DriverManager.getConnection(url, login, pwd);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Conexion establecida");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+			System.out.println("No tengo el driver cargado");
+		}
+
+	}
 
 	@Override
 	public HashMap<Integer, Deposito> obtenerDepositos() {
